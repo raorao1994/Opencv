db6090980b6ab00308df80e199ace2b74451d31f
扰扰 <lxh9788@qq.com>
2018-03-06 14:50:39 +0800

    识别特征点

---
+++ Feature_Test/Test02/Test02.cpp
@@ -0,0 +1,36 @@
+// Test02.cpp : 定义控制台应用程序的入口点。
+//SURF特征识别-特征点检测
+
+#include "stdafx.h"
+#include "opencv2/core.hpp" 
+#include <opencv2/imgcodecs.hpp>
+#include "opencv2/highgui.hpp" 
+#include "opencv2/features2d.hpp"  
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/xfeatures2d.hpp>
+#include <opencv2/xfeatures2d/nonfree.hpp>
+
+using namespace std;
+using namespace cv;
+using namespace cv::xfeatures2d;
+int main()
+{
+	Mat img = imread("D:/1.jpg");
+	
+	Ptr<SURF> surf = SURF::create(800);
+
+	vector<KeyPoint> key;
+
+	surf->detect(img,key);
+	Mat outImg;
+	drawKeypoints(img, key, outImg);
+
+	imshow("结果",outImg);
+
+	while (waitKey(0)==27)
+	{
+		
+	}
+    return 0;
+}
+
